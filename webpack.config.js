e8284f5d6cd9e3805488be3969a208443ca17efb
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-05-18 22:57:51 +0200

    fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,43 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
 module.exports = {
-	entry: "./src/app.js",
-	output: {
-		filename: "./dist/bundle.js",
-	},
+	mode: "development",
 	devServer: {
-		contentBase: "./dist",
+		open: true,
+		compress: true,
+		hot: true,
+		port: 8080,
+		historyApiFallback: true,
+		static: "./dist",
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-	],
+	entry: {
+		main: path.resolve(__dirname, "./src/app.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: "[name].bundle.js",
+		publicPath: "/",
+	},
 	module: {
-		loaders: [
+		rules: [
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
 			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "./src/index.html"), // template file
+			filename: "index.html", // output file
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+	],
 };
