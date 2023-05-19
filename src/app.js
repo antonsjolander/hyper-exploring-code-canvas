3722f26cd9fc852156d373484f92b98a78a5fd4d
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-05-19 15:20:17 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import * as dat from "dat.gui";
-
 const gui = new dat.GUI();
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const img = new Image();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+img.src =
+	"http://pixel.nymag.com/imgs/daily/vulture/2016/08/11/11-obama-sex-playlist.w529.h529.jpg";
+img.onload = pixelate;
+
+// ctx.mozImageSmoothingEnabled = false;
+// ctx.webkitImageSmoothingEnabled = false;
+// ctx.imageSmoothingEnabled = false;
+
+const blocks = {
+	value: 5,
+};
+
+gui.add(blocks, "value", 0, 50);
+
+function pixelate() {
+	const size = blocks.value * 0.01;
+	const w = canvas.width * size;
+	const h = canvas.height * size;
+
+	ctx.drawImage(img, 0, 0, canvas.width, canvas.width);
+	ctx.drawImage(canvas, 0, 0, w, h, 0, 0, canvas.width, canvas.height);
+}
