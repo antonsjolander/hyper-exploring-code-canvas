fe0a2f4f188a61925895afc7f3f5d4ee455d5452
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-05-19 13:44:05 +0200

    added dat gui

--- src/app.js
+++ src/app.js
@@ -11,21 +11,31 @@ const wave = {
 	y: canvas.height / 2,
 	length: 0.01,
 	amplitude: 100,
+	frequency: 0.01,
 };
 
 gui.add(wave, "y", 0, canvas.height);
 gui.add(wave, "length", -0.01, 0.01);
 gui.add(wave, "amplitude", -300, 300);
+gui.add(wave, "frequency", -0.01, 1);
 
+let increment = wave.frequency;
 function draw() {
 	requestAnimationFrame(draw);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.beginPath();
 	ctx.moveTo(0, canvas.height / 2);
 	for (let i = 0; i < canvas.width; i++) {
-		ctx.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude);
+		ctx.lineTo(
+			i,
+			wave.y +
+				Math.sin(i * wave.length + increment) *
+					wave.amplitude *
+					Math.sin(increment)
+		);
 	}
 	ctx.stroke();
+	increment += wave.frequency;
 }
 
 draw();
