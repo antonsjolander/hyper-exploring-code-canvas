ac7afeea973a1ee7fb49c1cd6f8e4698b386ae17
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-05-19 14:44:57 +0200

    last changes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import * as dat from "dat.gui";
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 const gui = new dat.GUI();
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const wave = {
 	y: canvas.height / 2,
@@ -20,26 +20,44 @@ const strokeColor = {
 	l: 50,
 };
 
+const backgroundColor = {
+	r: 0,
+	g: 0,
+	b: 0,
+	a: 0.01,
+};
+
 const waveFolder = gui.addFolder("wave");
 waveFolder.add(wave, "y", 0, canvas.height);
 waveFolder.add(wave, "length", -0.01, 0.01);
 waveFolder.add(wave, "amplitude", -300, 300);
 waveFolder.add(wave, "frequency", -0.01, 1);
 waveFolder.open();
-const strokeColorFolder = gui.addFolder("stroke color");
-strokeColorFolder.add(strokeColor, "h", 0, 255);
-strokeColorFolder.add(strokeColor, "s", 0, 100);
-strokeColorFolder.add(strokeColor, "l", 0, 100);
-strokeColorFolder.open();
+
+const strokeFolder = gui.addFolder("stroke");
+strokeFolder.add(strokeColor, "h", 0, 255);
+strokeFolder.add(strokeColor, "s", 0, 100);
+strokeFolder.add(strokeColor, "l", 0, 100);
+strokeFolder.open();
+
+const backgroundFolder = gui.addFolder("background");
+backgroundFolder.add(backgroundColor, "r", 0, 255);
+backgroundFolder.add(backgroundColor, "g", 0, 255);
+backgroundFolder.add(backgroundColor, "b", 0, 255);
+backgroundFolder.add(backgroundColor, "a", 0, 1);
+backgroundFolder.open();
 
 let increment = wave.frequency;
-function draw() {
-	requestAnimationFrame(draw);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-	ctx.moveTo(0, canvas.height / 2);
+function animate() {
+	requestAnimationFrame(animate);
+	c.fillStyle = `rgba(${backgroundColor.r}, ${backgroundColor.g}, ${backgroundColor.b}, ${backgroundColor.a})`;
+	c.fillRect(0, 0, canvas.width, canvas.height);
+
+	c.beginPath();
+	c.moveTo(0, canvas.height / 2);
+
 	for (let i = 0; i < canvas.width; i++) {
-		ctx.lineTo(
+		c.lineTo(
 			i,
 			wave.y +
 				Math.sin(i * wave.length + increment) *
@@ -47,11 +65,12 @@ function draw() {
 					Math.sin(increment)
 		);
 	}
-	ctx.stroke();
-	ctx.strokeStyle = `hsl(${Math.abs(strokeColor.h * Math.sin(increment))}, ${
+
+	c.strokeStyle = `hsl(${Math.abs(strokeColor.h * Math.sin(increment))}, ${
 		strokeColor.s
 	}%, ${strokeColor.l}%)`;
+	c.stroke();
 	increment += wave.frequency;
 }
 
-draw();
+animate();
