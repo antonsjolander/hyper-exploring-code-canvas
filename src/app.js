8fc4b5a32f9d02bcfcc3b3ebc51e6b525d9ef7ae
Anton Strandqvist <anton.sjolander@hyperisland.se>
2023-05-19 13:23:00 +0200

    added dat

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import * as dat from "dat.gui";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,8 +8,19 @@ canvas.height = window.innerHeight;
 ctx.beginPath();
 ctx.moveTo(0, canvas.height / 2);
 
+const gui = new dat.GUI();
+
+const wave = {
+	y: canvas.height / 2,
+	length: 0.01,
+	amplitude: 100,
+};
+
+gui.add(wave, "y", 0, canvas.height);
+gui.add(wave, "length", -0.01, 0.01);
+
 for (let i = 0; i < canvas.width; i++) {
-	ctx.lineTo(i, canvas.height / 2 + Math.sin(i * 0.01) * 100);
+	ctx.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude);
 }
 
 ctx.stroke();
